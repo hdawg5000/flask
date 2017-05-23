e7f1b8d73f02d6825df469f24246bc57bc3056d4
vagrant <vagrant@vagrant.vm>
2017-05-23 02:48:49 +0000

    added db files and modified code to print out restauarnts from db

--- project.py
+++ project.py
@@ -1,11 +1,31 @@
 from flask import Flask
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+#decorator - when server recevies request that matches arg,
+#proceeding method is run (hello world)
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
-    return "Hello World"
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += "</br>"
+    return output
 
-if __name__ == '__main__'
-    app.debug = True
-    app-run(host = '0.0.0.0', port = 5000)
+
+#if running from python interpreter, run following code
+if __name__ == '__main__':
+    app.debug = True #Helpful as server is restarted automatically each time code changes
+    app.run(host = '0.0.0.0', port = 5000)
