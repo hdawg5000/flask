0fff3ff9738f578f71aaff6eb08322bdb873ed8d
vagrant <vagrant@vagrant.vm>
2017-05-24 01:01:34 +0000

    create template directory for HTML code

--- project.py
+++ project.py
@@ -24,6 +24,23 @@ def restaurantMenu(restaurant_id):
         output += i.price + "</br>" + i.description + "</br></br>"
     return output
 
+@app.route('/restaurant/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+    output = ''
+    output += "<html><body><h1>Create a new restaurant</h1>"
+    output += "<form method='post' enctype='multipart/form-data'\
+     action='/restaurants/new/'>"
+    output += "<input type='text' placeholder"
+    return "page to create a new menu item. Task 1 complete!"
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+
 
 #if running from python interpreter, run following code
 if __name__ == '__main__':
